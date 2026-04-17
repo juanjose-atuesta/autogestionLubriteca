881d0b2099d32cf128e835d36189a00692ff04ff
Juan Jose Atuesta Flor <juanjoseatuestaflor@gmail.com>
2026-04-17 17:05:03 -0500

    intento de actualizar cada tanto

---
+++ js/actualizar.js
@@ -0,0 +1,28 @@
+
+
+function actualizar() {
+  fetch(API_BACKEND_URL + "customersList")
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === 'success') {
+        const clientes = data.customers;
+        setClientes(clientes);
+      }
+      fetch(API_BACKEND_URL + "listCustomersContacted")
+        .then(res => res.json())
+        .then(data => {
+          if (data.status === 'success') {
+            setContactados(data.customerList);
+          }
+          migrarClientesAHistorial();
+          actualizarStats();
+          mostrarAlertas();
+          actualizarBadgeContactados();
+          actualizarBadgeAgenda();
+        })
+
+    })
+
+}
+
+setTimeout(actualizar, 3000);
